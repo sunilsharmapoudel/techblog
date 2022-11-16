9214471003f6694990c059508ad70f4523b2f6df
Sunil <sunilpoudel1999@gmail.com>
2022-11-16 21:54:01 +0545

    Desktop Navigation Done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "font-sans",
+      },
+
+      colors: {
+        primary: "#212121",
+        secondary: "#cf2e2e",
+        tertiary: "#f2f2f2",
+        scrollbarcolor: "#858585",
+      },
+      screens: {
+        'xlg': '1150px',
+        // => @media (min-width: 1150px) { ... }
+      },
+    },
+  },
+  plugins: [],
+};
